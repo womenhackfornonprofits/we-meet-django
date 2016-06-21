d79fcc563e1189d365b47d22b558dc9d631b078b
bibiana <bcristofol@gmail.com>
2016-06-21 22:25:48 +0100

    Add models

---
+++ london_cafes/london_cafes/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.utils import timezone
+
+RATING_OPTIONS = (
+    (1, 'Not Good'),
+    (2, 'OK'),
+    (3, 'Good'),
+    (4, 'Very Good'),
+    (5, 'Excellent'),
+)
+
+
+class Address(models.Model):
+    street = models.TextField()
+    postcode = models.TextField()
+    city = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'addresses'
+
+
+class Ratings(models.Model):
+    rating = models.CharField(
+        max_length=30,
+        choices=RATING_OPTIONS,
+        default=5)
+
+    class Meta:
+        abstract = True
+
+
+class Atmosphere(Ratings):
+    pass
+
+
+class Coffee(Ratings):
+    pass
+
+
+class Wifi(Ratings):
+    pass
+
+
+class Cafe(models.Model):
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    address = models.ForeignKey(Address)
+    atmosphere = models.ForeignKey(Atmosphere)
+    coffee = models.ForeignKey(Coffee)
+    wifi = models.ForeignKey(Wifi)
