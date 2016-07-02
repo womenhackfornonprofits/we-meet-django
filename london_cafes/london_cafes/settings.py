8b3e970a20e23d71a165f2132581ac582274b1d3
Icicleta <icicleta@gmail.com>
2016-07-02 12:57:19 +0100

    intial homepage view

--- london_cafes/london_cafes/settings.py
+++ london_cafes/london_cafes/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'london_cafes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'london_cafes/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- london_cafes/london_cafes/urls.py
+++ london_cafes/london_cafes/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from views import HomepageView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomepageView.as_view(), name="index"),
 ]
