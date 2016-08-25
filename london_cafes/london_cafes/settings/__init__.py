c61724721b8cfcc3de6ad070442a7c56bdb13c1e
Liliana Kastilio <lili@lilianakastilio.co.uk>
2016-08-25 20:59:44 +0100

    Feature/cafe search (#44)
    * Added normalize and a searchbox to homepage
    
    * Front end setup and search commit
    
    * Fixes based on comments
    
    * Removed an extra bracket

--- london_cafes/london_cafes/settings/__init__.py
+++ london_cafes/london_cafes/settings/__init__.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,5 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/staticfiles/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'/staticfiles')
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
